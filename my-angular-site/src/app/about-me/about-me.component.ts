0cba6f46a6146385cf18a92ce8a419fe9327372c
Chris <ckim990@west-mec.org>
2019-12-17 15:26:27 -0700

    More API attempts

--- my-angular-site/src/app/about-me/about-me.component.ts
+++ my-angular-site/src/app/about-me/about-me.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { } from 'googlemaps';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss' ]
 })
 export class AboutMeComponent implements OnInit {
+  @ViewChild('map', {static: true}) mapElement: any;
+  map: google.maps.Map;
+  // map: google.maps.Map;
+  // mapElement: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    
   }
 
-}
+  ngOnInit(): void {
+    const mapProperties = {
+         center: new google.maps.LatLng(35.2271, -80.8431),
+         zoom: 15,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
+    
+ }
+  }
+
+
